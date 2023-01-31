03bb0b93541afda757c6f558a0a96766cdcab974
bushracmps <bushracmps@gmail.com>
2023-01-31 19:10:27 +0530

    initial project

---
+++ src/main/java/com/infy/springbootrestapicrud/app/SpringbootrestapicrudApplication.java
@@ -0,0 +1,13 @@
+package com.infy.springbootrestapicrud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestapicrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestapicrudApplication.class, args);
+	}
+
+}
